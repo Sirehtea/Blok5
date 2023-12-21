74afd09a99bcbcfc125547b9fee920e5a4c5d671
turgayfatih.yasar@hotmail.com <turgayfatih.yasar@hotmail.com>
2023-12-21 19:31:31 +0100

    add new feature: check multiple files for plagiarism

--- main.py
+++ main.py
@@ -52,6 +52,42 @@ def collect_lexicon(file_path):
 def should_compare(author1, author2, matrix_opmerkingen):
     return author1 != author2 and not matrix_opmerkingen[author2][author1]
 
+def compare_files_and_syntax_trees(file1_path, file2_path, spell_checker):
+    comparison_results = []
+
+    with open(file1_path, "r") as file1:
+        code1 = file1.read()
+
+    with open(file2_path, "r") as file2:
+        code2 = file2.read()
+
+    if compare_files(file1_path, file2_path):
+        comparison_results.append("identieke file " + os.path.basename(file1_path))
+
+    comments1 = extract_comments(file1_path)
+    comments2 = extract_comments(file2_path)
+    identical_comments = set(comments1) & set(comments2)
+
+    if identical_comments:
+        comparison_results.append("identieke comments: " + ", ".join(identical_comments))
+
+    lexicon1 = collect_lexicon(file1_path)
+    lexicon2 = collect_lexicon(file2_path)
+
+    misspelled_words1 = spell_checker.unknown(lexicon1)
+    misspelled_words2 = spell_checker.unknown(lexicon2)
+
+    identical_misspelled_words = set(misspelled_words1) & set(misspelled_words2)
+
+    if identical_misspelled_words:
+        comparison_results.append("identieke spelfouten: " + ", ".join(identical_misspelled_words))
+
+    if compare_csts_after_removing_comments(code1, code2):
+        comparison_results.append("identieke CST na verwijderen comments " + os.path.basename(file1_path))
+
+    return comparison_results if comparison_results else None
+
+
 def build_matrix(directory):
     authors = sorted(os.listdir(directory))
     matrix_opmerkingen = {author: {other_author: [] for other_author in authors} for author in authors}
@@ -61,44 +97,19 @@ def build_matrix(directory):
     for i, author1 in enumerate(authors):
         for j, author2 in enumerate(authors):
             if should_compare(author1, author2, matrix_opmerkingen):
-                file1_path = os.path.join(directory, author1, os.listdir(os.path.join(directory, author1))[0])
-                file2_path = os.path.join(directory, author2, os.listdir(os.path.join(directory, author2))[0])
+                files_author1 = sorted(os.listdir(os.path.join(directory, author1)))
+                files_author2 = sorted(os.listdir(os.path.join(directory, author2)))
 
-                with open(file1_path, "r") as file1:
-                    code1 = file1.read()
+                for file1, file2 in zip(files_author1, files_author2):
+                    file1_path = os.path.join(directory, author1, file1)
+                    file2_path = os.path.join(directory, author2, file2)
 
-                with open(file2_path, "r") as file2:
-                    code2 = file2.read()
+                    comparison_result = compare_files_and_syntax_trees(file1_path, file2_path, spell_checker)
 
-                if os.path.basename(file1_path) == os.path.basename(file2_path) and compare_files(file1_path, file2_path):
-                    matrix_opmerkingen[author1][author2].append("identieke file en naam " + os.path.basename(file1_path))
-                elif os.path.basename(file1_path) == os.path.basename(file2_path):
-                    matrix_opmerkingen[author1][author2].append("identieke naam " + os.path.basename(file1_path))
-                elif compare_files(file1_path, file2_path):
-                    matrix_opmerkingen[author1][author2].append("identieke file " + os.path.basename(file1_path))
+                    if comparison_result:
+                        matrix_opmerkingen[author1][author2].append(comparison_result)
 
-                comments1 = extract_comments(file1_path)
-                comments2 = extract_comments(file2_path)
-                identical_comments = set(comments1) & set(comments2)
-
-                if identical_comments:
-                    matrix_opmerkingen[author1][author2].append("identieke comments: " + ", ".join(identical_comments))
-
-                lexicon1 = collect_lexicon(file1_path)
-                lexicon2 = collect_lexicon(file2_path)
-
-                misspelled_words1 = spell_checker.unknown(lexicon1)
-                misspelled_words2 = spell_checker.unknown(lexicon2)
-
-                identical_misspelled_words = set(misspelled_words1) & set(misspelled_words2)
-
-                if identical_misspelled_words:
-                    matrix_opmerkingen[author1][author2].append("identieke spelfouten: " + ", ".join(identical_misspelled_words))
-
-                if compare_csts_after_removing_comments(code1, code2):
-                    matrix_opmerkingen[author1][author2].append("identieke CST na verwijderen comments " + os.path.basename(file1_path))
-
-        return authors, matrix_opmerkingen
+    return authors, matrix_opmerkingen
 
 def generate_html_output(authors, matrix_opmerkingen, output_filename):
     env = Environment(
