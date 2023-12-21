bde7cef59d16d333a6f864b42a8c396278b663fe
turgayfatih.yasar@hotmail.com <turgayfatih.yasar@hotmail.com>
2023-12-21 04:54:37 +0100

    add new feature: check for identical file names and/or identical content

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
+import os
+import filecmp
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-auteurs = ["Vincent", "Jos", "Yllias"]
+def compare_files(path1, path2):
+    return filecmp.cmp(path1, path2)
 
-alias_mapping = {auteur: f"student_{i}" for i, auteur in enumerate(auteurs)}
+def should_compare(author1, author2, matrix_opmerkingen):
+    return author1 != author2 and not matrix_opmerkingen[author2][author1]
 
-matrix_opmerkingen = {auteur: {andere_auteur: [] for andere_auteur in auteurs} for auteur in auteurs}
+def build_matrix(directory):
+    authors = sorted(os.listdir(directory))
+    matrix_opmerkingen = {author: {other_author: [] for other_author in authors} for author in authors}
 
-matrix_opmerkingen["Vincent"]["Jos"] = ["dezelfde verdachte fout"]
-matrix_opmerkingen["Jos"]["Yllias"] = ["vergelijkbare spellingsfouten"]
+    for i, author1 in enumerate(authors):
+        for j, author2 in enumerate(authors):
+            if should_compare(author1, author2, matrix_opmerkingen):
+                file1 = os.path.join(directory, author1, os.listdir(os.path.join(directory, author1))[0])
+                file2 = os.path.join(directory, author2, os.listdir(os.path.join(directory, author2))[0])
 
-env = Environment(
-    loader=FileSystemLoader("."),
-    autoescape=select_autoescape()
-)
+                if os.path.basename(file1) == os.path.basename(file2) and compare_files(file1, file2):
+                    matrix_opmerkingen[author1][author2].append("identieke file en naam " + os.path.basename(file1))
+                elif os.path.basename(file1) == os.path.basename(file2):
+                    matrix_opmerkingen[author1][author2].append("identieke naam " + os.path.basename(file1))
+                elif compare_files(file1, file2):
+                    matrix_opmerkingen[author1][author2].append("identieke file " + os.path.basename(file1))
 
-template = env.get_template("outputtemplate.html")
+    return authors, matrix_opmerkingen
 
-output_file_name = input("Hoe moet je output file heten? (zonder extensie) ")
+def generate_html_output(authors, matrix_opmerkingen, output_filename):
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape()
+    )
 
-with open(output_file_name + ".html", "w") as output_file:
-    output_file.write(template.render(auteurs=alias_mapping.values(), matrix_opmerkingen=matrix_opmerkingen, alias_mapping=alias_mapping))
+    template = env.get_template("outputtemplate.html")
+    output_html = template.render(authors=authors, matrix_opmerkingen=matrix_opmerkingen)
 
-print(f"De HTML-output is opgeslagen in {output_file_name}")
+    output_filepath = f"{output_filename}.html"
+    with open(output_filepath, "w") as output_file:
+        output_file.write(output_html)
+
+    print(f"HTML file generated: {output_filepath}")
+
+def main():
+    directory_path = input("Enter the path to the analysis directory: ")
+    output_filename = input("Enter the desired filename for the HTML output (without extension): ")
+
+    authors, matrix_opmerkingen = build_matrix(directory_path)
+    generate_html_output(authors, matrix_opmerkingen, output_filename)
+
+if __name__ == "__main__":
+    main()
